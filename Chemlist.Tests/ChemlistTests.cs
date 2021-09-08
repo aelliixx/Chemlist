9cf4fbbed745fba4ae4dc18c1ac014e4a5e62e13
AELLiiXX <mockus49@gmail.com>
2021-09-08 06:07:37 +0300

    Unit test naming compliance, removed the files!!!

--- Chemlist.Tests/ChemlistTests.cs
+++ Chemlist.Tests/ChemlistTests.cs
@@ -15,7 +15,7 @@ namespace Chemlist.Tests
 		[InlineData("Fe + O2 + H →  Fe(OH)2", "Fe + O2 + 2H → Fe(OH)2")]
 		[InlineData("SbCl3 + Cl2 → SbCl5", "SbCl3 + Cl2 → SbCl5")]
 		[InlineData("CH3CH2OH → CH2CH2 + H2O", "CH3CH2OH → CH2CH2 + H2O")]
-		public void balancer(String f, String exp)
+		public void balancer_ShouldWork(String f, String exp)
 		{
 			var returned = ChemicalObject.balance(f);
 			Assert.Equal(exp, returned);
